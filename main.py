5152799bd626ae4ca3adcd204842d50f1b1bddd7
Your name here <your_email@example.com>
2022-05-23 09:50:39 +0300

    fetch and return response

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+url = "https://bionic-reading1.p.rapidapi.com/convert"
+
+
+payload = {
+	"content":"Hello World",
+	"response_type":"html",
+	"request_type":"html",
+	"fixation":"1",
+	"saccade":"10"
+}
+headers = {
+	"content-type": "application/x-www-form-urlencoded",
+    "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com",
+	"X-RapidAPI-Key": "8d0ff07d7fmshebfc3ce9513926cp1da824jsna77a200e9cdf"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+print(response.text)
+
 @app.route('/')
 def home():
-    title = 'Home'
-    return render_template('Home.html', title = title)
+    return response.text
 
 if __name__ == '__main__':
     app.run( debug = True )
