17bfeada4aacef3d49d98fa725b59bc6964e1c42
Natty <nutzaccs@gmail.com>
2022-05-23 20:15:40 +0300

    styles

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    data = StringField('Text', validators=[DataRequired()])
+    data = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('Submit')
